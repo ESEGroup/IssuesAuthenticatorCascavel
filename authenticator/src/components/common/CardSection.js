2844fe23572a660ff48263ef7e332535e675865b
Pedro Reis <pedroreisv@gmail.com>
2017-10-30 20:10:27 -0200

    Add some common components

---
+++ authenticator/src/components/common/CardSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const CardSection = props => {
+  return (
+    <View style={[styles.containerStyle, props.style]}>{props.children}</View>
+  )
+}
+
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative',
+    marginTop: 5
+  }
+}
+
+export default CardSection
