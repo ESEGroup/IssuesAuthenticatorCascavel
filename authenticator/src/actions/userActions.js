1ef35b6a636feeb7711fb40b77be6740d731753b
Pedro Reis <pedroreisv@gmail.com>
2017-11-01 00:08:07 -0200

    Add auth types and actions

---
+++ authenticator/src/actions/userActions.js
@@ -0,0 +1,68 @@
+import {
+  USER_AUTH_PENDING,
+  USER_AUTH_ENTER_SUCCESS,
+  USER_AUTH_ENTER_FAIL,
+  USER_AUTH_LEAVE_SUCCESS,
+  USER_AUTH_LEAVE_FAIL
+} from './types'
+
+export const registerUserEnter = (userId, labId) => {
+  return dispatch => {
+    dispatch({ type: USER_AUTH_PENDING })
+
+    fetch('http://192.168.0.101:8080/registrar_entrada_authenticator', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId,
+        labId
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin'
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.erro) {
+          return userRegisterFail(dispatch, USER_AUTH_ENTER_FAIL)
+        }
+
+        return userRegisterSuccess(dispatch, USER_AUTH_ENTER_SUCCESS)
+      })
+      .catch(() => userRegisterFail(dispatch, USER_AUTH_ENTER_FAIL))
+  }
+}
+
+export const registerUserLeave = (userId, labId) => {
+  return dispatch => {
+    dispatch({ type: USER_AUTH_PENDING })
+
+    fetch('http://192.168.0.101:8080/registrar_saida_authenticator', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId,
+        labId
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin'
+    })
+      .then(res => {
+        if (res.erro) {
+          return userRegisterFail(dispatch, USER_AUTH_LEAVE_FAIL)
+        }
+
+        return userRegisterSuccess(dispatch, USER_AUTH_LEAVE_SUCCESS)
+      })
+      .catch(() => userRegisterFail(dispatch, USER_AUTH_LEAVE_FAIL))
+  }
+}
+
+const userRegisterSuccess = (dispatch, type) => {
+  dispatch({ type })
+}
+
+const userRegisterFail = (dispatch, type) => {
+  dispatch({ type })
+}
