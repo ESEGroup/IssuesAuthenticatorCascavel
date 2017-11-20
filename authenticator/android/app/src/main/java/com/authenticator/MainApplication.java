ae282abe2cae7c863745028bb2f26a81149eec8c
Pedro Reis <pedroreisv@gmail.com>
2017-11-20 21:49:50 -0200

    Add react-native-background-task dependency

--- authenticator/android/app/src/main/java/com/authenticator/MainApplication.java
+++ authenticator/android/app/src/main/java/com/authenticator/MainApplication.java
@@ -3,6 +3,7 @@ package com.authenticator;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
 import com.devstepbcn.wifi.AndroidWifiPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BackgroundTaskPackage(),
             new AndroidWifiPackage(),
             new VectorIconsPackage()
       );
@@ -45,5 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    BackgroundTaskPackage.useContext(this);
   }
 }
